1c2cd186e602bb7586b2dba7139656a27283e42e
camilyalmeida878-jpg <camilyalmeida878@gmail.com>
2025-09-09 14:04:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ const mensagens = [
   "Você e mais forte do que imagina 💛",
   "Nunca se esquesa: sua vida importa 🌻",
   "Você não eta sozinho, estamos juntos 🤝",
-  ""
+  "Acredite: dias melhores virõa ☀️",
+  "Sempre existe esperança 🌈"
+  ];
+
+document.getElementByid("btnMensagem").addEventlistener("click", () => {
+ const aleatoria = mensagem[Math.random() * mensagens.length)];
+ document.getElementByid("mensagem").innerText = aleatoria;
+});
