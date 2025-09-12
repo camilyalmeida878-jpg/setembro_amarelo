7fc73ccef4eb87803570b0a993439d1c3acdfa8b
camilyalmeida878-jpg <camilyalmeida878@gmail.com>
2025-09-12 08:38:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const mensagens = [
-  "Falar é a melhor solução. 🗣️"
-  "Você importa. Sua vida importa. 💛"
-  "Setembro Amarelo: a vida em primeiro lugar. 🌻"
-  "Acolher, ouvir, apoiar. 🤝"
-  "Não é drama, é dor. Cuide da saúde mental. 🧠"
-  "Dê voz à vida. 🎤"
-  "Juntos pela valorização da vida. 💬"
-  "Setembro é amarelo, mas o cuidado é o ano todo. 🕊️"
-  "Uma conversa pode salvar uma vida. 📞"
-  "Prevenção também é afeto. ❤️"
-  "A vida é a melhor escolha. 🌟"
-  "Você não está sozinho. 🤗"
-  "Falar salva vidas. 🧡"
-  "Amar é escutar sem julgar. 👂"
+  "Falar é a melhor solução. 🗣️",
+  "Você importa. Sua vida importa. 💛",
+  "Setembro Amarelo: a vida em primeiro lugar. 🌻",
+  "Acolher, ouvir, apoiar. 🤝",
+  "Não é drama, é dor. Cuide da saúde mental. 🧠",
+  "Dê voz à vida. 🎤",
+  "Juntos pela valorização da vida. 💬",
+  "Setembro é amarelo, mas o cuidado é o ano todo. 🕊️",
+  "Uma conversa pode salvar uma vida. 📞",
+  "Prevenção também é afeto. ❤️",
+  "A vida é a melhor escolha. 🌟",
+  "Você não está sozinho. 🤗",
+  "Falar salva vidas. 🧡",
+  "Amar é escutar sem julgar. 👂",
   "Seja luz na vida de alguém. ✨"
-  ];
+];
 
 document.getElementById("btnMensagem").addEventListener("click", () => {
- const aleatoria = mensagens[Math.floor(Math.random() * mensagens.length)];
- document.getElementById("mensagem").innerText = aleatoria;
+  const aleatoria = mensagens[Math.floor(Math.random() * mensagens.length)];
+  document.getElementById("mensagem").innerText = aleatoria;
 });
