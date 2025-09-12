7968a575997dbc421dfffd8a0f9b8ca8fdfa343d
camilyalmeida878-jpg <camilyalmeida878@gmail.com>
2025-09-12 08:36:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const mensagens = [
-  "Você e mais forte do que imagina 💛",
-  "Nunca se esquesa: sua vida importa 🌻",
-  "Você não eta sozinho, estamos juntos 🤝",
-  "Acredite: dias melhores virõa ☀️",
-  "Sempre existe esperança 🌈"
+  "Falar é a melhor solução. 🗣️"
+  "Você importa. Sua vida importa. 💛"
+  "Setembro Amarelo: a vida em primeiro lugar. 🌻"
+  "Acolher, ouvir, apoiar. 🤝"
+  "Não é drama, é dor. Cuide da saúde mental. 🧠"
+  "Dê voz à vida. 🎤"
+  "Juntos pela valorização da vida. 💬"
+  "Setembro é amarelo, mas o cuidado é o ano todo. 🕊️"
+  "Uma conversa pode salvar uma vida. 📞"
+  "Prevenção também é afeto. ❤️"
+  "A vida é a melhor escolha. 🌟"
+  "Você não está sozinho. 🤗"
+  "Falar salva vidas. 🧡"
+  "Amar é escutar sem julgar. 👂"
   ];
 
 document.getElementById("btnMensagem").addEventListener("click", () => {
